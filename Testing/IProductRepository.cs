7c30433dfb1c6cafaf72f06cb35c4b906cf1bb44
Brittany Ridley <britt.ridley@outlook.com>
2022-01-15 12:47:23 -0600

    ASPNET

---
+++ Testing/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Testing.Models;
+using System;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Testing
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+        
+        public Product GetProduct(int id);
+    }
+}
---
+++ Testing/ProductRepository.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Testing.Models;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using Dapper;
+
+namespace Testing
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _conn;
+         
+           public ProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("SELECT * FROM PRODUCTS;");
+        }
+
+        public Product GetProduct(int id)
+        {
+            return _conn.QuerySingle<Product>("SELECT * FROM PRODUCTS WHERE PRODUCTID = @id",
+                new { id = id });
+        }
+
+
+    }
+}
